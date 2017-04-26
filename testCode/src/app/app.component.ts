d90b61d2907aa8d8ce3b566052b4fe2ac80b0425
ambirdsall <ambirdsall@gmail.com>
2017-04-25 18:01:14 -0700

    Tweak title appearance

--- testCode/src/app/app.component.ts
+++ testCode/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'In Good Company'
+  title = 'Good Company'
   subtitle = 'Sigfig take-home project'
 }
